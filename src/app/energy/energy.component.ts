c7d282b13c14268d511facec06b5eb87b53b1ab5
yessine mkadem <yessinemkadem@yahoo.fr>
2022-10-07 12:40:35 +0200

    third commit

--- src/app/energy/energy.component.ts
+++ src/app/energy/energy.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-energy',
@@ -7,9 +8,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnergyComponent implements OnInit {
 
-  constructor() { }
+  public jouletocal = ''; // variabel will carry the joule to calorie input value
+  public caltojoule = ''; // vaiable will carry the calorie to jouel input value
+  public jouletokw = ''; // vaiable will carry the joule to kilowatt input value
+  public kwtojoule = ''; // vaiable will carry the kilowatt to joule input value
+
+  public formgroup !: FormGroup;
+
+  constructor(public formbuilder : FormBuilder) { }
 
   ngOnInit(): void {
+    this.formgroup = this.formbuilder.group({
+      jouleTocalories :'',
+      caloriesTojoule :'',
+      jouleTowatt : '',
+      wattTojoule :''
+    })
+  }
+
+  // define the convert from joule to calorie function
+  convertjouleTocal(){
+    var x = this.formgroup.get('jouletocal')?.value; // x will carry the joule to calorie input value
+    var y = this.formgroup.get('caltojoule')?.value; // y will carry the calorie to joule input value
+    // convert from joule to calories
+    if (x !== '' && y ===''){
+      this.caltojoule =(x / 4184).toString();
+    }
+    // convert from calorie to joule
+    else if (x === '' && y !== ''){
+      this.caltojoule =(y /  4184).toString();
+    }
+    // if there is no value don't covnert
+    else if (x === '' && y ==='')return
+    else return
+  }
+
+  //define the conert from  joule to kilowatt function
+  convertjouleTokilowatt(){
+    var x = this.formgroup.get('jouleTowatt')?.value; // x will carry the value of joule to kilowatt input
+    var y = this.formgroup.get('wattTojoule')?.value; // y will carry the value of watt to kilo watt input 
+    // convert from joule to kilowatt/hour 
+    if (x !== '' &&  y ===''){
+      this.kwtojoule = (x / 3600000).toString();
+    }
+    // convert from kilowatt/hour to joule
+    else if (x === '' && y !== ''){
+      this.jouletokw = (y / 3600000).toString();
+    }
+    // if there is no value don't convert
+    else if (x ==='' && y ==='')return
+    else return
+  }
+
+  // define the convert function to call all convert functions
+  convert(){
+    this.convertjouleTocal();
+    this.convertjouleTokilowatt();
+  }
+
+  // define the reset function to reset all input fields
+  reset(){
+    this.formgroup.reset();
   }
 
 }
--- src/app/power/power.component.ts
+++ src/app/power/power.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-power',
@@ -7,9 +8,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PowerComponent implements OnInit {
 
-  constructor() { }
+  public wtojoule = ''; // variable will carry the watt to joule input value
+  public jouletow = ''; // variable will carry the joule to watt input value
+  public wtojoulemin = ''; // variable will carry the watt to joule minute input value
+  public joulemintow = ''; // variable will carry the joule minute to watt input value
+  public wtojouleh =''; // variable will carry the watt to joule hour input value
+  public joulehtow =''; //variable will carry the joule hour to wat input value
+
+  public formgroup !: FormGroup;
+
+  constructor(public formbuilder :FormBuilder) { }
 
   ngOnInit(): void {
+    this.formgroup =  this.formbuilder.group({
+      wattTojoulesecond :'',
+      joulesecondTowatt :'',
+      wattTojouleminute :'',
+      jouleminuteTowatt :'',
+      wattTojoulehour :'',
+      joulehourTowatt :'' 
+    })
+  }
+
+  // define the convet from watt to joule/second function
+  convertwattTojoulesec(){
+    var x = this.formgroup.get('wattTojoulesecond')?.value; // x will carry the watt to joule input value
+    var y = this.formgroup.get('joulesecondTowatt')?.value; // y will carry the joule to watt input value
+    // convert from watt to joule
+    if (x !== '' && y ===''){
+      this.jouletow = (x * 1).toString();
+    }
+    // convert from joule to watt
+    else if (x ==='' && y !== ''){
+      this.wtojoule = (y * 1).toString();
+    }
+    // if there is no value don't convert
+    else if  (x === '' && y === '')return
+    else return
+  }
+
+  // define the convert from joule/minute 
+  convertwattTojoulemin(){
+    var x = this.formgroup.get('wattTojouleminute')?.value; // x will carry the watt to joule minute input value
+    var y = this.formgroup.get('jouleminuteTowatt')?.value; // y will carry the joule minute to watt input value
+    // convert from watt to joule minute
+    if (x !== '' && y === ''){
+      this.wtojoulemin =(x * 60).toString();
+    }
+    // convert from joule minute to watt
+    else if (x ==='' && y !== ''){
+      this.joulemintow = ( y / 60).toString();
+    }
+    // if there is no value don't convert
+    else if (x === '' && y === '')return
+    else return
+  }
+
+  // define the convert from watt to joule hour function
+  convertwattTojoulehour(){
+    var x = this.formgroup.get('wattTojoulehour')?.value; // x will carry the watt to joule hour input value
+    var y = this.formgroup.get('joulehourTowatt')?.value; // y will carry the joule hour to watt input value
+    // convert from watt to joule/hour
+    if (x !== '' && y ===''){
+      this.joulehtow = (x * 3600).toString();
+    }
+    // convert from joule hour to watt
+    else if (x === '' && y !== ''){
+      this.wtojouleh =(y / 3600).toString();
+    }
+    // if there is no value don't convert
+    else if (x === '' && y === '')return
+    else return
+  }
+
+  // define the convert function that call all convert functions
+  convert(){
+    this.convertwattTojoulehour();
+    this.convertwattTojoulemin();
+    this.convertwattTojoulesec();
+  }
+
+  // define the reset function to reset all input fields
+  reset(){
+    this.formgroup.reset();
   }
 
 }
